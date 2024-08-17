6e67810eeb77f17403f5364e164e9a3588ebb3a2
Nazar <nazar.iurchak@gmail.com>
2024-08-17 19:17:48 +0300

    code improvements

--- main.py
+++ main.py
@@ -2,91 +2,115 @@ import base64
 
 import requests
 from rich.console import Console
+from rich.prompt import Confirm
 
 TIMEOUT_IN_SECONDS = 10
+
+# CLIENT_ID:SECRET
+CLIENT = b"98f7e42c2e3a4f86a74eb43fbb41ed39:0a2449a2-001a-451e-afec-3e812901c4d7"
+
 
 class DeleteFriends:
     def __init__(self) -> None:
         # https://github.com/MixV2/EpicResearch/blob/master/docs/auth/auth_clients.md
         self.client = base64.b64encode(
-            b"98f7e42c2e3a4f86a74eb43fbb41ed39:0a2449a2-001a-451e-afec-3e812901c4d7",
+            CLIENT,
         ).decode(
             "utf-8",
         )
 
-        self.console = Console()
+        self.session = requests.Session()
 
-        token = self.create_token()
-        self.auth_link, self.device_code = self.device_code(token)
-
-    def run(self) -> bool:
-        self.console.clear()
-        self.console.print("[yellow bold]Epic Games Store Friends Remover")
-
-        self.console.input(
-            f"[green bold]Verify login [/green bold][yellow](click enter to continue)[/yellow][green bold]:[/green bold] [red]{self.auth_link}",
-        )
-        data = self.device_code_verify(self.device_code)
-
-        if not data["success"]:
-            self.console.print(f"[red][bold][ERROR][/bold] {data['error_message']}")
-            return False
-
-        self.data = data
-
-        self.console.print(f"[green]Successfully logged in [bold]{self.data['display_name']}")
-
-        self.console.print("[green]Removing Friends...")
-
-        friends_count = self.get_friend_count()
-        self.delete_friends()
-
-        self.console.print(
-            f"[green bold]Removed {friends_count} friends![/green bold] [yellow]You can kill all sessions to reset token.",
-        )
-
-        self.console.input()
-
-        return None
-
-    # Reference: https://github.com/MixV2/EpicResearch/blob/master/docs/auth/grant_types/client_credentials.md
-    def create_token(self) -> str:
-        headers = {
+        self.session.headers.update = {
             "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": f"basic {self.client}",
-        }
-        body = {
-            "grant_type": "client_credentials",
         }
 
-        data = requests.post(
+        self.account_id = None
+        self.bearer = None
+
+    def run(self) -> None:
+        console = Console()
+        console.clear()
+
+        console.print("[yellow bold]Epic Games Store Friends Remover")
+
+        token = self.token()["access_token"]
+
+        device_code = self.device_code(token)
+
+        console.input(
+            f"[bold][green]Login (press enter to continue):[/green] [red]{device_code["verification_uri_complete"]}",
+        )
+
+        try:
+            data = self.device_code_verify(device_code["device_code"])
+        except requests.HTTPError:
+            console.print("[red]User not logged in!")
+            return
+
+        self.account_id = data["account_id"]
+        self.bearer = data["access_token"]
+
+        console.print(f"[bold][green]Successfully logged in [blue]{data['displayName']}")
+
+        friends = self._get_friends()
+
+        friends_count = len(friends["friends"])
+
+        confirm = Confirm.ask(
+            f"[bold][yellow]You have [blue]{friends_count} [yellow]friends! "
+            "You sure you want to remove all friends?",
+        )
+
+        if not confirm:
+            return
+
+        self._remove_friends()
+
+        self._kill_session()
+
+        console.print(
+            f"[bold][green]Removed {friends_count} friends!\n[yellow]Session has been killed",
+        )
+
+        console.input()
+
+    def token(self) -> dict:
+        """https://github.com/MixV2/EpicResearch/blob/master/docs/auth/grant_types/client_credentials.md"""
+        headers = {"Authorization": f"basic {self.client}"}
+
+        body = {"grant_type": "client_credentials"}
+
+        response = self.session.post(
             "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token",
             headers=headers,
             data=body,
             timeout=TIMEOUT_IN_SECONDS,
-        ).json()
+        )
 
-        return data["access_token"]
+        response.raise_for_status()
 
-    # Reference: https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/AccountService/Authentication/DeviceCode/Create.md
-    def device_code(self, token) -> tuple:
+        return response.json()
+
+    def device_code(self, token: str) -> dict:
+        """https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/AccountService/Authentication/DeviceCode/Create.md"""
         headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": f"bearer {token}",
         }
 
-        data = requests.post(
-            "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/deviceAuthorization",
+        response = self.session.post(
+            "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/deviceAuthorization",
             headers=headers,
             timeout=TIMEOUT_IN_SECONDS,
-        ).json()
+        )
 
-        return data["verification_uri_complete"], data["device_code"]
+        response.raise_for_status()
 
-    # Reference: https://github.com/MixV2/EpicResearch/blob/master/docs/auth/grant_types/device_code.md
+        return response.json()
+
     def device_code_verify(self, device_code) -> dict:
+        """https://github.com/MixV2/EpicResearch/blob/master/docs/auth/grant_types/device_code.md"""
         headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": f"basic {self.client}",
         }
 
@@ -95,55 +119,60 @@ class DeleteFriends:
             "device_code": device_code,
         }
 
-        data = requests.post(
+        response = self.session.post(
             "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token",
             headers=headers,
             data=body,
             timeout=TIMEOUT_IN_SECONDS,
-        ).json()
+        )
 
-        if "errorCode" in data:
-            return {
-                "success": False,
-                "error_code": data["errorCode"],
-                "error_message": data["errorMessage"],
-            }
+        response.raise_for_status()
 
-        return {
-            "success": True,
-            "display_name": data["displayName"],
-            "account_id": data["account_id"],
-            "access_token": data["access_token"],
-        }
+        return response.json()
 
-    # Reference: https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/FriendsService/Friends/FriendsList.md
-    def get_friend_count(self) -> int:
+    def _get_friends(self) -> int:
+        """https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/FriendsService/Friends/FriendsList.md"""
         headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": f"bearer {self.data['access_token']}",
+            "Authorization": f"bearer {self.bearer}",
         }
 
-        data = requests.get(
-            f"https://friends-public-service-prod.ol.epicgames.com/friends/api/v1/{self.data['account_id']}/summary",
-            headers=headers,
-            timeout=TIMEOUT_IN_SECONDS,
-        ).json()
-
-        return len(data["friends"])
-
-    # Reference: https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/FriendsService/Friends/Clear.md
-    def delete_friends(self) -> None:
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": f"bearer {self.data['access_token']}",
-        }
-
-        requests.delete(
-            f"https://friends-public-service-prod.ol.epicgames.com/friends/api/v1/{self.data["account_id"]}/friends",
+        response = self.session.get(
+            f"https://friends-public-service-prod.ol.epicgames.com/friends/api/v1/{self.account_id}/summary",
             headers=headers,
             timeout=TIMEOUT_IN_SECONDS,
         )
 
+        response.raise_for_status()
+
+        return response.json()
+
+    def _remove_friends(self) -> None:
+        """https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/FriendsService/Friends/Clear.md"""
+        headers = {
+            "Authorization": f"bearer {self.bearer}",
+        }
+
+        self.session.delete(
+            f"https://friends-public-service-prod.ol.epicgames.com/friends/api/v1/{self.account_id}/friends",
+            headers=headers,
+            timeout=TIMEOUT_IN_SECONDS,
+        )
+
+    def _kill_session(self) -> None:
+        """https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/FriendsService/Friends/Clear.md"""
+        headers = {
+            "Authorization": f"bearer {self.bearer}",
+        }
+
+        self.session.delete(
+            f"https://account-public-service-prod.ol.epicgames.com/account/api/oauth/sessions/kill/{self.bearer}",
+            headers=headers,
+            timeout=TIMEOUT_IN_SECONDS,
+        )
+
+def main() -> None:
+    friends_remover = DeleteFriends()
+    friends_remover.run()
 
 if __name__ == "__main__":
-    app = DeleteFriends().run()
+    main()
