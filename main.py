a7f891c48486b3a4e83dc6424e5ad8c9a49cc0b2
Nazar <nazar.iurchak@gmail.com>
2024-12-02 15:56:14 +0200

    add accepter

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import base64
 
 import requests
 from rich.console import Console
-from rich.prompt import Confirm
+from rich.prompt import Confirm, Prompt
+# from itertools import batched
 
 TIMEOUT_IN_SECONDS = 10
 
 # CLIENT_ID:SECRET
 CLIENT = b"98f7e42c2e3a4f86a74eb43fbb41ed39:0a2449a2-001a-451e-afec-3e812901c4d7"
 
+console = Console()
+console.clear()
 
 class FriendsRemover:
     def __init__(self) -> None:
@@ -25,9 +28,6 @@ class FriendsRemover:
         self.bearer = None
 
     def run(self) -> None:
-        console = Console()
-        console.clear()
-
         console.print("[yellow bold]Epic Games Store Friends Remover")
 
         token = self.token()["access_token"]
@@ -47,11 +47,30 @@ class FriendsRemover:
         self.account_id = data["account_id"]
         self.bearer = data["access_token"]
 
+
         console.print(f"[bold][green]Successfully logged in [blue]{data['displayName']}")
 
-        friends = self._get_friends()
+        choice = Prompt.ask("[bold][yellow]You want to run?", choices=["Remover", "Accepter"], default="Remover")
 
-        friends_count = len(friends["friends"])
+        try:
+            match choice:
+                case "Remover":
+                    self.run_remover()
+                case "Accepter":
+                    self.run_accepter()
+        finally:
+            self._kill_session()
+
+            console.print(
+                "[bold][yellow]Session has been killed",
+            )
+
+            console.input()
+
+    def run_remover(self) -> None:
+        summary = self._get_summary()
+
+        friends_count = len(summary["friends"])
 
         confirm = Confirm.ask(
             f"[bold][yellow]You have [blue]{friends_count} [yellow]friends! "
@@ -62,13 +81,23 @@ class FriendsRemover:
             self._remove_friends()
             console.print(f"[bold][green]Removed {friends_count} friends!")
 
-        self._kill_session()
+    def run_accepter(self) -> None:
+        summary = self._get_summary()
 
-        console.print(
-            "[bold][yellow]Session has been killed",
+        incoming_requests = summary["incoming"]
+        incoming_requests_count = len(incoming_requests)
+
+        confirm = Confirm.ask(
+            f"[bold][yellow]You have [blue]{incoming_requests_count} [yellow]incoming requests! "
+            "You sure you want to accept them all?",
         )
 
-        console.input()
+        if confirm:
+            # for batch in batched(incoming_requests, 100):
+            #     self._accept_bulk_incoming_friend_requests([acc["accountId"] for acc in batch])
+            self._accept_bulk_incoming_friend_requests([acc["accountId"] for acc in incoming_requests])
+            console.print(f"[bold][green]Added {incoming_requests_count} friends!")
+
 
     def token(self) -> dict:
         """https://github.com/MixV2/EpicResearch/blob/master/docs/auth/grant_types/client_credentials.md"""
@@ -121,12 +150,11 @@ class FriendsRemover:
             timeout=TIMEOUT_IN_SECONDS,
         )
 
-        print(response.text)
         response.raise_for_status()
 
         return response.json()
 
-    def _get_friends(self) -> dict:
+    def _get_summary(self) -> dict:
         """https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/FriendsService/Friends/FriendsList.md"""
         headers = {
             "Authorization": f"bearer {self.bearer}",
@@ -135,6 +163,25 @@ class FriendsRemover:
         response = self.session.get(
             f"https://friends-public-service-prod.ol.epicgames.com/friends/api/v1/{self.account_id}/summary",
             headers=headers,
+            timeout=TIMEOUT_IN_SECONDS,
+        )
+
+        response.raise_for_status()
+
+        return response.json()
+
+    def _accept_bulk_incoming_friend_requests(self, ids: list[str] | tuple[str]) -> list[str]:
+        """https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/FriendsService/Friends/AcceptBulk.md"""
+        headers = {
+            "Authorization": f"bearer {self.bearer}",
+        }
+        params = {
+            "targetIds": ",".join(ids),
+        }
+        response = self.session.post(
+            f"https://friends-public-service-prod.ol.epicgames.com/friends/api/v1/{self.account_id}/incoming/accept",
+            headers=headers,
+            params=params,
             timeout=TIMEOUT_IN_SECONDS,
         )
 
